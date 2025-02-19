69dec67d7a633f8cfb015defd8151bd3a15e8934
guettafa <irontitou5@gmail.com>
2025-02-18 21:18:43 -0500

    Replaced vector for a unordered_map

--- Bo1ESP/src/dllmain.cpp
+++ Bo1ESP/src/dllmain.cpp
@@ -1,47 +1,30 @@
 #include "pch.h"
 #include "client.h"
 #include "hook.h"
+
 #include <iostream>
+#include <unordered_map>
 
 #pragma warning(disable: 4996)
 
 uintptr_t codeCaveAddrs;
 uintptr_t entityAddrs = 0;
-
 uintptr_t localPlayerEntityAddrs = 0x1a796f8;
 
-std::vector<uintptr_t> entities{};
-bool isAlreadyHere = false;
+std::unordered_map<uintptr_t, bool> entities{};
 
 __declspec(naked) void HookFunc()
 {
-    std::printf("HOOKED \n");
-
     _asm
     {
         mov [entityAddrs],esi
     }
 
-    if (entityAddrs != localPlayerEntityAddrs)
-    {
-        for (uintptr_t entity : entities)
-        {
-            if (entityAddrs != entity)
-                continue;
+    if (!entities[entityAddrs] && entityAddrs != localPlayerEntityAddrs)
+        entities[entityAddrs] += 1;
 
-            isAlreadyHere = true; 
-            std::printf(" Entity is already here: %x\n", entityAddrs);
-
-        }
-
-        if (!isAlreadyHere)
-        {
-            entities.push_back(entityAddrs);
-            isAlreadyHere = false;
-        }
-        std::printf("Size of vector : %d\n", entities.size());
-    }
-
+    std::printf("size : %d\n", entities.size());
+    
     _asm
     {
         jmp [codeCaveAddrs]
